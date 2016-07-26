b03b41c16294339f693443d7f6ffd745c5605d79
tao.huang <tao.huang@juphoon.com.cn>
2016-07-26 18:00:56 +0800

    feature: 增加sqlite3前

--- youMuTou/Classes/Login/Controller/YMLoginAndRegisteController.m
+++ youMuTou/Classes/Login/Controller/YMLoginAndRegisteController.m
@@ -7,8 +7,10 @@
 //
 
 #import "YMLoginAndRegisteController.h"
+#import "YMNetWorkManager.h"
+#import "HomeViewController.h"
 
-@interface YMLoginAndRegisteController ()
+@interface YMLoginAndRegisteController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
 
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
@@ -20,14 +22,72 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
+
 - (IBAction)seeBtnClick:(id)sender {
     
-}
-- (IBAction)registerBtnClick:(id)sender {
     
 }
+
+- (IBAction)registerBtnClick:(id)sender {
+    if (_userNameTextField.text.length != 0 && ![_userNameTextField.text isEqualToString:@""] && _passwordTextField.text.length != 0  &&![_passwordTextField.text isEqualToString:@""]) {
+        //[self postRegistrtRequestWithUsername:_userNameTextField.text password:_passwordTextField.text];
+    }
+    
+}
+
+- (void)postRegistrtRequestWithUsername:(NSString *)username password:(NSString *)password
+{
+    NSString *url = @"http://127.0.0.1:5000/user_register";
+    YMNetWorkManager *manager = [YMNetWorkManager sharedManager];
+    NSMutableDictionary * dict = [NSMutableDictionary dictionary];
+    dict[@"username"] = username;
+    dict[@"token"] = password;
+    
+    [manager postRequestWithParameter:dict url:url successBlock:^(id responseBody) {
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示"message:@"注册成功" preferredStyle:  UIAlertControllerStyleAlert];
+        [self presentViewController:alert animated:true completion:nil];
+        
+    } failureBlock:^(NSString *error) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示"message:@"该用户名已经注册" preferredStyle:  UIAlertControllerStyleAlert];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }]];
+        
+        //弹出提示框；
+        [self presentViewController:alert animated:true completion:nil];
+        
+    }];
+
+}
+
+- (void)postLoginRequestWithUser:(NSString *)username password:(NSString *)password
+{
+    NSString *url =  @"http://127.0.0.1:5000/user_login";
+    YMNetWorkManager *manager = [YMNetWorkManager sharedManager];
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    dict[@"username"] = username;
+    dict[@"token"] = password;
+    [manager postRequestWithParameter:dict url:url successBlock:^(id responseBody) {
+        [UIApplication sharedApplication].keyWindow.rootViewController = [[HomeViewController alloc] init];
+    } failureBlock:^(NSString *error) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示"message:@"用户名或密码不正确" preferredStyle:  UIAlertControllerStyleAlert];
+        [self presentViewController:alert animated:true completion:nil];
+    }];
+
+}
+
 - (IBAction)loginBtnClick:(id)sender {
     
+    
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.view endEditing:YES];
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,14 +95,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
